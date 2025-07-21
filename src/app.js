1dfe8c73aa53c3b2221ea818dc626fb964c0efaf
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-07-21 16:32:06 +0530

    update todo

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json())
 
 let todo = [];
-let todoCount = 1;
+let todoCount = 0;
 
 app.post('/todo',(req,res)=>{
     const{task, completed} = req.body;
@@ -27,7 +27,7 @@ app.post('/todo',(req,res)=>{
     todo.push(newtodo);
     res.status(201).json({
         message:"task is added",
-        todo: newtodo
+        // todo: newtodo
     })
 })
 
@@ -35,6 +35,31 @@ app.get("/todo", (req,res)=>{
     res.status(200).json({
         message:"task fetch successfully",
         todo
+    })
+})
+
+app.patch("/todo/:id", (req,res)=>{
+    const id = parseInt(req.params.id)
+    const {task} = req.body;
+
+    const index = todo.findIndex(item =>item.id === id)
+
+    if(index === -1){
+        return  res.status(404).json({
+            message : "task not found"
+        })
+    }
+
+    if(!task){
+        return res.status(400).json({
+            message: "task is required"
+        })
+    }
+
+    todo[index].task = task
+    res.status(200).json({
+        message : "task updated successfully",
+        todo: todo[index]
     })
 })
 
